80f742a5035fd024ff601e9b81140f21570eed0c
Ritesh Ramesh Vishwakarma <vishwakarmaritesh253@gmail.com>
2025-06-09 11:40:46 +0530

    started

---
+++ src/test/java/com/Ritesh/Project/LetsPlayTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ritesh.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsPlayTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
